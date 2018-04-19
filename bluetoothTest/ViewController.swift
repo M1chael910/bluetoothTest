ef838eb39b2cde662c9e0f5688194ff37b2439a2
Michael Murphy <mpmurphy1432@gmail.com>
2018-04-18 19:07:01 -0500

    First Commit

--- bluetoothTest/ViewController.swift
+++ bluetoothTest/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralManagerDelegate {
+    
+    var manager: CBManager!
+    var peripheral: CBPeripheral!
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    
+    var centralManager: CBCentralManager!
+        override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        manager = CBCentralManager(delegate: self, queue: nil)
+        
+    }
+    
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        central.scanForPeripherals(withServices: nil, options: nil)
+        
+    }
+    
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        var output = Label.text! + "\n"
+        view.backgroundColor = .red
+        if let device = (advertisementData as NSDictionary)
+            .object(forKey: CBAdvertisementDataLocalNameKey)
+            as? String {
+            output += device
+        }
+        Label.text = output
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        
+    }
+    
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        
+    }
+    
+    
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 
 }
+
+
+
+        
 
